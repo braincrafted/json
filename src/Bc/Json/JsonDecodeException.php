3050e88b656e624aa6aca7631eb7f1c54d2f9f21
Florian Eckerstorfer <florian@theroadtojoy.at>
2013-04-23 23:42:51 +0200

    Added PHPDoc

--- src/Bc/Json/JsonDecodeException.php
+++ src/Bc/Json/JsonDecodeException.php
@@ -1,7 +1,20 @@
 <?php
+/**
+ * This file is part of BcJson.
+ *
+ * (c) 2013 Florian Eckerstorfer
+ */
 
 namespace Bc\Json;
 
+/**
+ * JsonDecodeException
+ *
+ * @package   BcJson
+ * @author    Florian Eckerstorfer <florian@eckerstorfer.co>
+ * @copyright 2013 Florian Eckerstorfer
+ * @license   http://opensource.org/licenses/MIT The MIT License
+ */
 class JsonDecodeException extends \Exception
 {
 }
